21b2fdc468b0841a4c7340c098c914843e7e4637
safchik <safchik9@gmail.com>
2024-02-06 11:22:57 +0000

    subscribe section completed

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,11 +2,12 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
 
 export {
     Button,
     Nav,
     ShoeCard,
     PopularProductCard,
-
+    ReviewCard
 }
