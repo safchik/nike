0d8dd287e12b770ebf5e28fc8d9f1922ddcf22e3
safchik <safchik9@gmail.com>
2024-02-06 11:06:41 +0000

    review card component added

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -15,8 +15,14 @@ const CustomerReviews = () => {
             </p>
 
             <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
-                {reviews.map((review, index) => (
-                    <ReviewCard />
+                {reviews.map((review) => (
+                    <ReviewCard
+                        key={review.customerName}
+                        imgURL={review.imgURL}
+                        customerName={review.customerName}
+                        rating={review.rating}
+                        feedback={review.feedback}
+                    />
                 ))}
             </div>
         </section>
